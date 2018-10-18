87101acc92d2f38970007680aa1fbb2a762d4cbf
Maximiliano García Rozado <mrozado@lagash.com>
2018-10-18 11:31:20 -0300

    First commit

---
+++ seeders/20181018131942-Users.js
@@ -0,0 +1,33 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+*/
+    Example: return queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          firstName: "John",
+          lastName: "Doe",
+          email: "jhon@doe.com",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
